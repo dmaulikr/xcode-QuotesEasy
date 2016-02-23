232a6c34703ccc16cd809494516f39ded5240c63
Phil Ramirez <ramirezp6856@gmail.com>
2016-02-22 20:05:39 -0500

    Initial Commit

---
+++ QuotesEasy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuotesEasy
+//
+//  Created by Phillip Ramirez on 2/22/16.
+//  Copyright © 2016 philramirez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
